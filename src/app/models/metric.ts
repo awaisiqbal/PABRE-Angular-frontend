efccc53c831ec82f4035fdc3748f1659017ffbb3
Awais Iqbal <awais.iqbal2@gmail.com>
2018-12-15 23:11:23 +0100

    Added patterns details

--- src/app/models/metric.ts
+++ src/app/models/metric.ts
@@ -1,4 +1,9 @@
 export class Metric {
   id: number;
   name: string;
+  description: string;
+  type: string;
+  simple = class {
+    name: string;
+  };
 }
--- src/app/models/pattern.ts
+++ src/app/models/pattern.ts
@@ -1,4 +1,35 @@
+import {Metric} from './metric';
+import {Keyword} from './keyword';
+
 export class Pattern {
   id: number;
   name: string;
+  description: string;
+  author: string;
+  available: boolean;
+  editable: boolean;
+  keywords: string[];
+  versions = class {
+    id: number;
+    author: string;
+    goal: string;
+    forms = class {
+      id: number;
+      name: string;
+      description: string;
+      comments: string;
+      modificationDate: string;
+      fixedPart = class {
+        id: number;
+        formText: string;
+        questionText: string;
+        parameters = class {
+          id: number;
+          name: string;
+          description: string;
+          metric: Metric;
+        };
+      };
+    };
+  };
 }
